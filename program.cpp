d54b7501239f86070fa34addb1ed5850be78d4d8
frostbane <frostbane@programmer.net>
2025-06-26 10:38:22 +0900

    get working directory c-style

--- program.cpp
+++ program.cpp
@@ -1,9 +1,30 @@
+// #include <filesystem> // C++17
+#include <direct.h>
+
 #include "program.hpp"
+
+static
+std::string getWorkingDir()
+{
+    const size_t size = 1024;
+    char path[size] = {0};
+
+    if (getcwd(path, size) != nullptr)
+    {
+        return std::string(path);
+    }
+    else
+    {
+        return "";
+    }
+}
 
 void
 Program::run(std::string path,
              std::vector<std::string> list)
 {
+    std::cout << getWorkingDir() << std::endl;
+    // std::cout << std::filesystem::current_path() << std::endl // C++17
     std::cout << "path: " << path << std::endl
               << "arguments:" << std::endl;
 
