17647e38ff101d358686fdc08c3bd989bdd62693
frostbane <frostbane@programmer.net>
2025-06-26 11:45:50 +0900

    add linux support

--- arguments.cpp
+++ arguments.cpp
@@ -1,6 +1,17 @@
+#if defined(_WIN32) || defined(_WIN64) || defined(__WINDOWS__)
+#   define _LOSER_
+#else
+#   undef _LOSER_
+#endif
+
+#include <stdio.h> // FILENAME_MAX
 #include <string>
 // #include <filesystem> // C++17
+#ifdef _LOSER_
 #include <direct.h>
+#else
+#   include <unistd.h>
+#endif
 
 #include "arguments.hpp"
 
@@ -17,8 +28,7 @@ std::string getWorkingDir()
 {
     // return std::filesystem::current_path(); // C++17
 
-    const size_t size = 1024;
-    std::string path(size, '\0');
+    std::string path(FILENAME_MAX, '\0');
 
     if (getcwd(&path[0], path.capacity()) != nullptr)
     {
