ef307d77c11f573ec699115c0422ebcb0bac9f82
theurbanwanderess <shasheecosca@gmail.com>
2025-06-27 09:59:20 +0900

    refactoring

--- program.cpp
+++ program.cpp
@@ -2,23 +2,24 @@
 
 void
 Program::printInfo()
-    {
-    std::cout << "working directory: " << args.pwd << std::endl
-              << "path: " << args.path << std::endl;
+{
+    std::cout << "dir\t" << args.pwd << std::endl
+              << "path\t" << args.path << std::endl;
 }
 
 void
 Program::printArgs()
 {
-    std::cout << "arguments:" << std::endl;
+    std::cout << "count\t" << args.list.size() << std::endl;
+    std::cout << "arguments" << std::endl;
 
     int i = 1;
 
     for (auto& item: args.list)
     {
-        std::cout << "  " << i << ": " << item << std::endl;
+        std::cout << i << "\t" << item << std::endl;
         i++;
-}
+    }
 }
 
 Program::Program(Arguments args) :
